20e42937ee366d29fe890916f3a5fc9a1dbc27f5
Paul Kinlan <paul.kinlan@gmail.com>
2013-11-13 14:05:40 +0000

    Adding first commit

---
+++ slide_config.js
@@ -0,0 +1,27 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'Mobile Web UX',
+    subtitle: 'The Future of Your Sites',
+    eventTitle: 'Chrome Dev Summit',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/chrome-logo-tiny.png',
+
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Paul Kinlan',
+    company: 'Developer Advocate, Google',
+    gplus: 'http://google.com/+PaulKinlan',
+    twitter: '@paul_kinlan',
+    www: 'http://paul.kinlan.me',
+    github: 'http://github.com/paulkinlan'
+  }]
+};
+
