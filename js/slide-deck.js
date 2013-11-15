999880c20122ee4268ccf6ea881fcb1dae1a7495
Paul Kinlan <paul.kinlan@gmail.com>
2013-11-15 17:04:42 +0000

    Adding more

--- js/slide-deck.js
+++ js/slide-deck.js
@@ -319,11 +319,11 @@ SlideDeck.prototype.loadConfig_ = function(config) {
     if (settings.eventTitle) {
       document.title +=  ' - ' + settings.eventTitle;
     }
-    document.querySelector('[data-config-title]').innerHTML = settings.title;
+    //document.querySelector('[data-config-title]').innerHTML = settings.title;
   }
 
   if (settings.subtitle) {
-    document.querySelector('[data-config-subtitle]').innerHTML = settings.subtitle;
+    //document.querySelector('[data-config-subtitle]').innerHTML = settings.subtitle;
   }
 
   if (this.config_.presenters) {
--- slide_config.js
+++ slide_config.js
@@ -2,16 +2,12 @@ var SLIDE_CONFIG = {
   // Slide settings
   settings: {
     title: 'Mobile Web UX',
-    subtitle: 'The Future of Your Sites',
     eventTitle: 'Chrome Dev Summit',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
-    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/chrome-logo-tiny.png',
-
-    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+    favIcon: 'images/chrome-logo-tiny.png'
   },
 
   // Author information
